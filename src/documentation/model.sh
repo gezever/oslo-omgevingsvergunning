84ead4dc86b96c7f02c287eab9cd09e767978b29
gezever <gezever@skynet.be>
2024-10-14 18:41:56 +0200

    model

--- src/documentation/model.sh
+++ src/documentation/model.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-  sparql --results=TTL --data=../results/dwh_mapping/radar_oslo.ttl  --query model.rq  > model.ttl
+riot /home/gehau/git/OSLO/oslo-omgevingsvergunning/src/results/publicatie/* > /tmp/test.nt
+  sparql --results=TTL --data=/tmp/test.nt --query model.rq  > model.ttl
   rdf2dot  model.ttl | dot -Tpng > model.png
   rdf2dot  model.ttl  > model.dot
